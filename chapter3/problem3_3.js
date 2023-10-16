e087d8eaa79b32d4dccf1946b3de542013c76a0e
Italiz <italizv@gmail.com>
2023-10-16 15:49:26 -0400

    Finish chapter 3 problems

--- chapter3/problem3_3.js
+++ chapter3/problem3_3.js
@@ -1,1 +1,24 @@
+// Bean Counting
+function countBs(str) {
+  let count = 0
+  for (const ele of str) {
+    if (ele === "B") {
+      count++
+    }
+  }
+  return count
+}
 
+function countChar(str) {
+  let count = 0;
+  for (const ele of str) {
+    if (ele === "k") {
+      count++;
+    }
+  }
+  return count;
+}
+console.log(countBs("BBC"));
+// → 2
+console.log(countChar("kakkerlak", "k"));
+// → 4
