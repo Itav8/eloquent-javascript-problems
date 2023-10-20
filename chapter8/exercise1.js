f22315ac5be0eaf9102bd4fde7d8baa0540288cc
Italiz <italizv@gmail.com>
2023-10-20 19:16:05 -0400

    Finish chapter8

---
+++ chapter8/exercise1.js
@@ -0,0 +1,26 @@
+// Retry
+class MultiplicatorUnitFailure extends Error {}
+
+function primitiveMultiply(a, b) {
+  if (Math.random() < 0.2) {
+    //20%
+    return a * b;
+  } else {
+    throw new MultiplicatorUnitFailure("Klunk"); // 80%
+  }
+}
+
+function reliableMultiply(a, b) {
+  for (;;) {
+    try {
+      return primitiveMultiply(a, b);
+    } catch (e) {
+      if (!(e instanceof MultiplicatorUnitFailure)) {
+        throw e;
+      }
+    }
+  }
+}
+
+console.log(reliableMultiply(8, 8));
+// → 64
